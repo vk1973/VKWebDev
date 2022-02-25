faf20f608050f805486708bb4f4d626ae1707331
VK <viswanath.kaushik@gmail.com>
2022-02-25 09:23:10 -0600

    Java Exercises
    Java

---
+++ AddTwoNumbers.java
@@ -0,0 +1,7 @@
+public class AddTwoNumbers {
+  public static void main(Strings[] args){
+    int num1 = 10;
+    int num2 = 20;
+    System.out.println("Sum of two numbers ", num1, "and ", num2, "= ", num1+num2 );
+}
+}
